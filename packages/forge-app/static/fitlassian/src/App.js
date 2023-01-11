8d67892168f848e2dd6772a0b25f85bb399c756b
kunal528 <64953335+kunal528@users.noreply.github.com>
2023-01-12 00:02:33 +0530

    added variant switching in react

--- packages/forge-app/static/fitlassian/src/App.js
+++ packages/forge-app/static/fitlassian/src/App.js
@@ -4,6 +4,21 @@ import Rive from "@rive-app/react-canvas";
 
 function App() {
   const [storyPoints, setStoryPoints] = useState(null);
+  const [level, setLevel] = useState(1)
+  const levels = {
+    10: 'level10.riv',
+    9: 'level09.riv',
+    8: 'level08.riv',
+    7: 'level07.riv',
+    6: 'level06.riv',
+    5: 'level05.riv',
+    4: 'level04.riv',
+    3: 'level03.riv',
+    2: 'level02.riv',
+    1: 'level01.riv'
+  }
+
+  const BASE_URL = "https://raw.githubusercontent.com/DaoWise/fitlassian/main/assets/";
 
   useEffect(() => {
     invoke("get-story-points").then(setStoryPoints);
@@ -13,16 +28,16 @@ function App() {
     return <div>Loading...</div>;
   }
 
-  return (
-    <div
-      style={{
-        height: "90vh",
-        width: "100vw",
-      }}
-    >
-      <Rive src="https://cdn.rive.app/animations/vehicles.riv" />
 
-      <div>{storyPoints?.toString()??0}</div>
+
+  return (
+    <div key={level}>
+      <Rive src={`${BASE_URL}${levels[level]}`} style={{ height: "90vh", width: "100vw" }} />
+      <div style={{display:'flex'}}>
+        {Object.keys(levels).map((level, index) => {
+          return <button onClick={() => setLevel(index + 1)}>Level {index + 1}</button>
+        })}
+      </div>
     </div>
   );
 }
