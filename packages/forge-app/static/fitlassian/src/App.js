78aef0fb1931b14dce677513ea17c1be6b7d0826
Maadhav Sharma <maadhav2001@gmail.com>
2023-01-11 10:03:26 +0530

    update App.js

--- packages/forge-app/static/fitlassian/src/App.js
+++ packages/forge-app/static/fitlassian/src/App.js
@@ -1,28 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { invoke } from '@forge/bridge';
+import React, { useEffect, useState } from "react";
+import { invoke } from "@forge/bridge";
 import { Rive } from "@rive-app/canvas";
-
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-
-    new Rive({
-      // Hosted .riv asset, or a local one. Uncomment line 8 to try with
-      // a local basketball.riv asset, or add your own!
-      src: "https://cdn.rive.app/animations/vehicles.riv",
-      // src: "basketball.riv",
-      canvas: document.getElementById("canvas"),
-      autoplay: true
-    });
+    invoke("getText", { example: "my-invoke-variable" }).then(setData);
   }, []);
 
+  if (!data) {
+    return (
+      <Card>
+        <LoadingContainer>
+          <Spinner size="large" />
+        </LoadingContainer>
+      </Card>
+    );
+  }
   return (
-    <div>
-      {data ? data : 'Loading...'}
-      <canvas id="canvas" />
+    <div
+      style={{
+        height: "100%",
+      }}
+    >
+      <Rive src="https://cdn.rive.app/animations/vehicles.riv" />
     </div>
   );
 }
