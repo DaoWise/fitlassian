7e832641b84786b57bc6375c3c5fdaafa8f28cdc
Shadab Khan <shadab.2005@gmail.com>
2023-01-16 00:24:37 +0530

    fix(next): responsiveness

--- packages/next-app/components/Motivation.jsx
+++ packages/next-app/components/Motivation.jsx
@@ -5,14 +5,13 @@ import Img2 from "../public/assets/first.png";
 export const Motivation = () => {
   return (
     <div id="pitch">
-      <div className="p-36 mx-36 my-10">
+      <div className="md:p-12 my-10 flex justify-center items-center w-full">
         <iframe
-          width="884"
-          height="497"
           src="https://www.youtube.com/embed/cXda6JOUwbw"
           title="FitLassian - Gamifying Scrum Sprints"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowfullscreen
+          className="w-full h-full md:h-[497px] md:w-[884px]"
         ></iframe>
       </div>
     </div>
